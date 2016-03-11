1eec1aa44bc2eeaac85f175a0eeafa77d160a2a0
Nacho González-Garilleti <nachox7@outlook.com>
2016-03-11 19:42:13 +0100

    Práctica 2.1 en la que trabajamos con las clases Jarra y Estadistica. El ejercicio de la Jarra nos pedía que en el segundo ejemplo dejasemos una de las dos jarras (una de 5 L y otra de 7 L), con un litro dentro de si misma

---
+++ Test/Example.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Nacho González-Garilleti on 7/3/16.
+ */
+public class Example {
+
+    public static void main(String[] args) {  
+
+        System.out.println("Hello World, example test");  
+
+    }
+     
+}
+
