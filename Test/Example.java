980311a5b89171da737b07c16b8e43246edda455
Nacho González-Garilleti <nachox7@outlook.com>
2016-03-11 19:29:54 +0100

    Revert "Práctica 2.1 en la que trabajamos con las clases Jarra y Estadistica. El ejercicio de la Jarra nos pedía que en el segundo ejemplo dejasemos una de las dos jarras (una de 5 L y otra de 7 L), con un litro dentro de si misma"
    This reverts commit 2609b49da7af226f13a27770aeda15259edd8bdb.

--- Test/Example.java
+++ Test/Example.java
@@ -5,36 +5,7 @@ public class Example {
 
     public static void main(String[] args){
 
-        sumaTodo objeto = new sumaTodo();
-        sumaTodo objeto2 = new sumaTodo();
-
-        for(int i = 0;i <= 100;i++){
-
-            objeto.empieza();
-
-        }
-
-        for(int i = 0;i <= 100;i++){
-
-            // Al ser estatica la variable suma, se mantiene su valor a lo largo de la creacion
-            // de nuevos objetos
-
-            objeto2.empieza();
-
-        }
-
-    }
-
-}
-
-class sumaTodo{
-
-    private static int suma = 0;
-
-    public static void empieza(){
-
-        suma++;
-        System.out.println(suma);
+        System.out.println("Hello World, example test");
 
     }
 
